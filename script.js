271ed0300a8571a297a5e4d8fe66caf45a79b950
Roman <kavoriya@gmail.com>
2023-02-03 19:16:43 +0500

    Build: validate passwords using colors

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Float labels
+
 const FloatLabel = (() => {
 
   const handleFocus = (e) => {
@@ -12,7 +14,7 @@ const FloatLabel = (() => {
     }  
   };
 
-  const bindEvents = (element) => {
+  const bindEventsForFloats = (element) => {
     const floatField = element.querySelector('input');
     floatField.addEventListener('focus', handleFocus);
     floatField.addEventListener('blur', handleBlur);  
@@ -22,7 +24,7 @@ const FloatLabel = (() => {
     const floatContainers = document.querySelectorAll('.float-container');
     
     floatContainers.forEach((element) => {
-      bindEvents(element);
+      bindEventsForFloats(element);
     });
   };
 
@@ -31,3 +33,51 @@ const FloatLabel = (() => {
   };
 })();
 FloatLabel.init();
+
+// Validate passwords
+
+let passwordOne = document.querySelector("#pw-1");
+let passwordTwo = document.querySelector("#pw-2");
+
+function makePasswordsGreen() {
+  passwordOne.parentNode.classList.remove("invalid")
+  passwordOne.parentNode.classList.add("valid")
+  passwordTwo.parentNode.classList.remove("invalid")
+  passwordTwo.parentNode.classList.add("valid")
+}
+
+function makePasswordsRed() {
+  passwordOne.parentNode.classList.remove("valid")
+  passwordOne.parentNode.classList.add("invalid")
+  passwordTwo.parentNode.classList.remove("valid")
+  passwordTwo.parentNode.classList.add("invalid")
+}
+
+function makePasswordsGrey() {
+  passwordOne.parentNode.classList.remove("invalid")
+  passwordOne.parentNode.classList.remove("valid")
+  passwordTwo.parentNode.classList.remove("invalid")
+  passwordTwo.parentNode.classList.remove("valid")
+}
+
+
+function handleBlurPasswords() {
+  if (passwordOne.value!='' && passwordTwo.value!='' && passwordOne.value!=passwordTwo.value) {
+    makePasswordsRed();
+  } else if (passwordOne.value!='' && passwordTwo.value!='' && passwordOne.value==passwordTwo.value) {
+    makePasswordsGreen();
+  } else if (passwordOne.value=='' && passwordTwo.value!='') {
+    makePasswordsGrey();
+  } else if (passwordOne.value!='' && passwordTwo.value=='') {
+    makePasswordsGrey();
+  }
+};
+
+const bindEventsForPasswords = (element) => {
+  element.addEventListener('blur', handleBlurPasswords);
+}
+
+bindEventsForPasswords(passwordOne);
+bindEventsForPasswords(passwordTwo);
+
+
