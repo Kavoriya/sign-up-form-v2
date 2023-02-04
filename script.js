eac1af8ee86b15e85365435a02faf4a6b18e7fa7
Roman <kavoriya@gmail.com>
2023-02-04 13:33:18 +0500

    Build: validate all fields using color

--- script.js
+++ script.js
@@ -34,50 +34,69 @@ const FloatLabel = (() => {
 })();
 FloatLabel.init();
 
+// Validate email
+
+let email = document.querySelector(".email");
+email.addEventListener('blur', handleBlurEmail);
+
+function handleBlurEmail() {
+  let re = new RegExp(/.+@.+\..+/);
+  if (email.value!='' && re.test(email.value)) {
+    makeFieldGreen(email);
+  } else if (email.value == '') {
+    makeFieldGrey(email);
+  } else {
+    makeFieldRed(email);
+  }
+}
+
+// Validate username
+
+let username = document.querySelector(".username");
+username.addEventListener('blur', handleBlurUsername);
+
+function handleBlurUsername() {
+  if (username.value!='' && (username.value.length < 3 || username.value.length > 30)) {
+    makeFieldRed(username);
+  } else if (username.value == '') {
+    makeFieldGrey(username);
+  } else {
+    makeFieldGreen(username);
+  }
+}
+
 // Validate passwords
 
-let passwordOne = document.querySelector("#pw-1");
-let passwordTwo = document.querySelector("#pw-2");
-
-function makePasswordsGreen() {
-  passwordOne.parentNode.classList.remove("invalid")
-  passwordOne.parentNode.classList.add("valid")
-  passwordTwo.parentNode.classList.remove("invalid")
-  passwordTwo.parentNode.classList.add("valid")
-}
-
-function makePasswordsRed() {
-  passwordOne.parentNode.classList.remove("valid")
-  passwordOne.parentNode.classList.add("invalid")
-  passwordTwo.parentNode.classList.remove("valid")
-  passwordTwo.parentNode.classList.add("invalid")
-}
-
-function makePasswordsGrey() {
-  passwordOne.parentNode.classList.remove("invalid")
-  passwordOne.parentNode.classList.remove("valid")
-  passwordTwo.parentNode.classList.remove("invalid")
-  passwordTwo.parentNode.classList.remove("valid")
-}
-
+let passwordOne = document.querySelector(".password-1");
+let passwordTwo = document.querySelector(".password-2");
+passwordOne.addEventListener('blur', handleBlurPasswords);
+passwordTwo.addEventListener('blur', handleBlurPasswords);
 
 function handleBlurPasswords() {
   if (passwordOne.value!='' && passwordTwo.value!='' && passwordOne.value!=passwordTwo.value) {
-    makePasswordsRed();
+    makeFieldRed(passwordOne);
+    makeFieldRed(passwordTwo);
   } else if (passwordOne.value!='' && passwordTwo.value!='' && passwordOne.value==passwordTwo.value) {
-    makePasswordsGreen();
-  } else if (passwordOne.value=='' && passwordTwo.value!='') {
-    makePasswordsGrey();
-  } else if (passwordOne.value!='' && passwordTwo.value=='') {
-    makePasswordsGrey();
+    makeFieldGreen(passwordOne);
+    makeFieldGreen(passwordTwo);
+  } else if ((passwordOne.value=='' && passwordTwo.value!='') || (passwordOne.value!='' && passwordTwo.value=='')) {
+    makeFieldGrey(passwordOne);
+    makeFieldGrey(passwordTwo);
   }
-};
-
-const bindEventsForPasswords = (element) => {
-  element.addEventListener('blur', handleBlurPasswords);
 }
 
-bindEventsForPasswords(passwordOne);
-bindEventsForPasswords(passwordTwo);
+function makeFieldGreen(field) {
+  field.parentNode.classList.remove("invalid")
+  field.parentNode.classList.add("valid")
+}
 
+function makeFieldRed(field) {
+  field.parentNode.classList.remove("valid")
+  field.parentNode.classList.add("invalid")
+}
+
+function makeFieldGrey(field) {
+  field.parentNode.classList.remove("invalid")
+  field.parentNode.classList.remove("valid")
+}
 
